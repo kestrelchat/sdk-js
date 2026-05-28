df2daa9ef5c70a954c82566f148d7f6f3bdcbce0
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2026-05-28 04:20:13 -0500

    refactor: rename index.ts to client.ts
    Signed-off-by: Neo <77405346+networkpenetrationtester@users.noreply.github.com>

---
+++ src/client.ts
@@ -0,0 +1,22 @@
+import { $SessionResponse } from "./types/schemas";
+
+export interface KestrelClientOptions {
+  base_url?: string;
+}
+
+export class KestrelClient {
+  private sessions: $SessionResponse = [];
+  private base_url: string;
+
+  constructor(options: KestrelClientOptions = {}) {
+    this.base_url = options.base_url ?? 'https://kestrel.chat/api';
+  }
+
+  public async ping(): Promise<string> {
+    return 'pong';
+  }
+
+  public getApiUrl(): string {
+    return this.base_url;
+  }
+}
