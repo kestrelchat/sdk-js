5ddde729ac621cc38a257c44b2eadf29a18c54ee
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2026-05-29 15:33:45 -0500

    feat: reflect new feats in backend
    Signed-off-by: Neo <77405346+networkpenetrationtester@users.noreply.github.com>

--- src/api_wrapper.ts
+++ src/api_wrapper.ts
@@ -1,30 +1,48 @@
-import { FetchWrapper } from './fetch_wrapper';
-import type { $FetchWrapperArgs } from './fetch_wrapper';
+import { FetchWrapper } from './fetch_wrapper.ts';
+import type { $FetchWrapperArgs } from './fetch_wrapper.ts';
 import type {
   $ChangePasswordRequest,
+  $ConfirmEnableTotpRequest,
+  $DisableTotpRequest,
+  $EnableTotpResponse,
   $Error,
+  $LoginMfaStepRequest,
   $LoginRequest,
   $LoginResponse,
+  $LoginResponseDefault as $LoginResponseSuccess,
   $Meta,
   $RegisterRequest,
   $RegisterResponse,
   $SessionResponse,
 } from './types/schemas';
 
-export type $SuccessOrError<T> = Promise<
+export type $APIResponse<T> = Promise<
   { success: true; result: T } | { success: false; result: $Error }
 >;
 
 export async function ResponseToJSON<T>(
   response: Response,
-): $SuccessOrError<T> {
+): $APIResponse<T> {
   let result;
   try {
     result = await response.json();
     return { success: response.ok, result };
-  } catch {
-    return { success: response.ok, result: undefined as never }; // I don't know anymore
+  } catch (e) {
+    return {
+      success: false, result: {
+        error: {
+          code: response.statusText,
+          status: response.status,
+          message: `[ResponseToJSON] Failed to parse response. ${e}`
+        },
+        request_id: '-1'
+      }
+    };
   }
+}
+
+export function BEARER_AUTH(auth_token: string) {
+  return { authorization: `Bearer ${auth_token}` }
 }
 
 export const APPLICATION_JSON = {
@@ -48,38 +66,31 @@ export const DEFAULT_POST_WRAPPER_ARGS: $FetchWrapperArgs = {
   handler: ResponseToJSON,
 };
 
-export async function GetMeta(base_url: string): $SuccessOrError<$Meta> {
+export const DEFAULT_DELETE_WRAPPER_ARGS: $FetchWrapperArgs = {
+  init: {
+    headers: APPLICATION_JSON,
+    method: 'DELETE',
+  },
+  handler: ResponseToJSON,
+};
+
+export async function GetMeta(base_url: string): $APIResponse<$Meta> {
   return await FetchWrapper(`${base_url}/`, DEFAULT_GET_WRAPPER_ARGS);
 }
 
-export async function GetUserCount(base_url: string): $SuccessOrError<number> {
+export async function GetUserCount(base_url: string): $APIResponse<number> {
   return await FetchWrapper(
     `${base_url}/users/count`,
     DEFAULT_GET_WRAPPER_ARGS,
   );
 }
 
-export async function GetSessions(
-  base_url: string,
-  auth_token: string,
-): $SuccessOrError<$SessionResponse> {
-  return await FetchWrapper(`${base_url}/auth/sessions`, {
-    handler: ResponseToJSON,
-    init: {
-      ...DEFAULT_GET_WRAPPER_ARGS.init,
-      headers: {
-        authorization: `Bearer ${auth_token}`,
-      },
-    },
-  });
-}
-
 export async function Register(
   base_url: string,
   register_request: $RegisterRequest,
-): $SuccessOrError<$RegisterResponse> {
+): $APIResponse<$RegisterResponse> {
   return await FetchWrapper(`${base_url}/auth/register`, {
-    handler: ResponseToJSON,
+    ...DEFAULT_POST_WRAPPER_ARGS,
     init: {
       ...DEFAULT_POST_WRAPPER_ARGS.init,
       body: JSON.stringify(register_request),
@@ -90,9 +101,9 @@ export async function Register(
 export async function Login(
   base_url: string,
   login_request: $LoginRequest,
-): $SuccessOrError<$LoginResponse> {
+): $APIResponse<$LoginResponse> {
   return await FetchWrapper(`${base_url}/auth/login`, {
-    handler: ResponseToJSON,
+    ...DEFAULT_POST_WRAPPER_ARGS,
     init: {
       ...DEFAULT_POST_WRAPPER_ARGS.init,
       body: JSON.stringify(login_request),
@@ -100,15 +111,85 @@ export async function Login(
   });
 }
 
+export async function LoginMfaStep(
+  base_url: string,
+  login_mfa_step_request: $LoginMfaStepRequest,
+): $APIResponse<$LoginResponseSuccess> {
+  return await FetchWrapper(`${base_url}/auth/login/mfa`, {
+    ...DEFAULT_POST_WRAPPER_ARGS,
+    init: {
+      ...DEFAULT_POST_WRAPPER_ARGS.init,
+      body: JSON.stringify(login_mfa_step_request),
+    },
+  });
+}
+
 export async function ChangePassword(
   base_url: string,
   change_password_request: $ChangePasswordRequest,
-): $SuccessOrError<void> {
+): $APIResponse<void> {
   return await FetchWrapper(`${base_url}/auth/password/change`, {
-    handler: ResponseToJSON,
+    ...DEFAULT_POST_WRAPPER_ARGS,
     init: {
       ...DEFAULT_POST_WRAPPER_ARGS.init,
       body: JSON.stringify(change_password_request),
+    },
+  });
+}
+
+export async function EnableTotp(
+  base_url: string,
+  // auth_token: string
+): $APIResponse<$EnableTotpResponse> {
+  return await FetchWrapper(`${base_url}/auth/mfa/totp`, {
+    ...DEFAULT_POST_WRAPPER_ARGS,
+    init: {
+      // headers: BEARER_AUTH(auth_token),
+      ...DEFAULT_POST_WRAPPER_ARGS.init,
+      body: null,
+    },
+  });
+}
+
+export async function DisableTotp(
+  base_url: string,
+  disable_totp_request: $DisableTotpRequest,
+  // auth_token: string
+): $APIResponse<void> {
+  return await FetchWrapper(`${base_url}/auth/mfa/totp`, {
+    ...DEFAULT_DELETE_WRAPPER_ARGS,
+    init: {
+      // headers: BEARER_AUTH(auth_token),
+      body: JSON.stringify(disable_totp_request),
+    },
+    ...DEFAULT_DELETE_WRAPPER_ARGS,
+  });
+}
+
+export async function ConfirmEnableTotp(
+  base_url: string,
+  confirm_enable_totp_request: $ConfirmEnableTotpRequest,
+  // auth_token: string
+): $APIResponse<void> {
+  return await FetchWrapper(`${base_url}/auth/mfa/totp/confirm`, {
+    ...DEFAULT_POST_WRAPPER_ARGS,
+    init: {
+      ...DEFAULT_POST_WRAPPER_ARGS.init,
+      // headers: BEARER_AUTH(auth_token),
+      body: JSON.stringify(confirm_enable_totp_request),
+    },
+  });
+}
+
+export async function GetSessions(
+  base_url: string,
+  auth_token: string,
+): $APIResponse<$SessionResponse> {
+  return await FetchWrapper(`${base_url}/auth/sessions`, {
+    ...DEFAULT_GET_WRAPPER_ARGS,
+    init: {
+      ...DEFAULT_GET_WRAPPER_ARGS.init,
+      headers: BEARER_AUTH(auth_token),
     },
   });
 }
