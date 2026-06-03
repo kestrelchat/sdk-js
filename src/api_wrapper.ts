93a50035f3036bb495fe726c567cd80888e0c0ab
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2026-06-02 23:03:57 -0500

    feat: finish migrating api wrapper to class
    Signed-off-by: Neo <77405346+networkpenetrationtester@users.noreply.github.com>

--- src/api_wrapper.ts
+++ src/api_wrapper.ts
@@ -25,33 +25,36 @@ export interface $APIError {
 }
 
 export interface $APIResponseSuccess<T> {
-  success: true,
-  result: T
+  success: true;
+  result: T;
 }
 
 export interface $APIResponseFail {
-  success: false,
-  result: $APIError
+  success: false;
+  result: $APIError;
 }
 
-export type $APIResponse<T> = Promise<$APIResponseSuccess<T> | $APIResponseFail>;
+export type $APIResponse<T> = Promise<
+  $APIResponseSuccess<T> | $APIResponseFail
+>;
 
 export async function APIResponseToJSON<T>(
   response: Response,
 ): Promise<$APIResponse<T>> {
   try {
-    if (response.ok) return { success: true, result: await response.json() }
+    if (response.ok) return { success: true, result: await response.json() };
     else throw new Error('Request failed');
   } catch (e) {
     return {
-      success: false, result: {
+      success: false,
+      result: {
         error: {
           code: response.statusText,
           status: response.status,
-          message: `[ResponseToJSON] Failed to parse response. ${e}`
+          message: `[ResponseToJSON] Failed to parse response. ${e}`,
         },
-        request_id: '-1'
-      }
+        request_id: '-1',
+      },
     };
   }
 }
@@ -60,18 +63,19 @@ export async function APIResponseToString(
   response: Response,
 ): Promise<$APIResponse<string>> {
   try {
-    if (response.ok) return { success: true, result: await response.text() }
+    if (response.ok) return { success: true, result: await response.text() };
     else throw new Error('Request failed');
   } catch (e) {
     return {
-      success: false, result: {
+      success: false,
+      result: {
         error: {
           code: response.statusText,
           status: response.status,
-          message: `[ResponseToString] Failed to parse response. ${e}`
+          message: `[ResponseToString] Failed to parse response. ${e}`,
         },
-        request_id: '-1'
-      }
+        request_id: '-1',
+      },
     };
   }
 }
@@ -80,182 +84,176 @@ export async function APIResponseEmpty(
   response: Response,
 ): Promise<$APIResponse<undefined>> {
   try {
-    if (response.ok) return { success: true, result: undefined }
+    if (response.ok) return { success: true, result: undefined };
     else throw new Error('Request failed');
   } catch (e) {
     return {
-      success: false, result: {
+      success: false,
+      result: {
         error: {
           code: response.statusText,
           status: response.status,
-          message: `[ResponseToString] Failed to parse response. ${e}`
+          message: `[ResponseToString] Failed to parse response. ${e}`,
         },
-        request_id: '-1'
-      }
+        request_id: '-1',
+      },
     };
   }
 }
 
-export async function GetMeta(base_url: string) {
-  return await FetchHandler<$APIResponse<$Meta>>(
-    new ModularRequest(`${base_url}/`)
-    , APIResponseToJSON
-  );
-}
+export class API {
+  private session_id!: string;
+  private auth_token!: string;
+  private base_url: string;
 
-export async function GetUserCount(base_url: string) {
-  return await FetchHandler<$APIResponse<number>>(
-    new ModularRequest(`${base_url}/users/count`)
-    , APIResponseToJSON
-  )
-}
+  constructor(base_url: string) {
+    this.base_url = base_url;
+    // PromptLogin -> this.session_id = session_id;
+  }
 
-export async function Register(
-  base_url: string,
-  register_request: $RegisterRequest,
-) {
-  return await FetchHandler<$APIResponse<$RegisterResponse>>(
-    new ModularRequest(`${base_url}/auth/register`)
-      .POST
-      .ApplicationJson
-      .Body(register_request)
-    , APIResponseToJSON
-  );
-}
+  async GetMeta() {
+    return await FetchHandler<$APIResponse<$Meta>>(
+      new ModularRequest(`${this.base_url}/`),
+      APIResponseToJSON,
+    );
+  }
 
-export async function Login(
-  base_url: string,
-  login_request: $LoginRequest,
-) {
-  return await FetchHandler<$APIResponse<$LoginResponse>>(
-    new ModularRequest(`${base_url}/auth/login`)
-      .POST
-      .ApplicationJson
-      .Body(login_request)
-    , APIResponseToJSON
-  );
-}
+  async GetUserCount() {
+    return await FetchHandler<$APIResponse<number>>(
+      new ModularRequest(`${this.base_url}/users/count`),
+      APIResponseToJSON,
+    );
+  }
 
-export async function LoginMfaStep(
-  base_url: string,
-  login_mfa_step_request: $LoginMfaStepRequest,
-) {
-  return await FetchHandler<$APIResponse<$LoginResponseSuccess>>(
-    new ModularRequest(`${base_url}/auth/login/mfa`)
-      .POST
-      .ApplicationJson
-      .Body(login_mfa_step_request)
-    , APIResponseToJSON
-  )
-}
+  async PromptLogin() {
+    console.log('You are not logged in. Please log in!');
+  }
 
-export async function Logout(
-  base_url: string,
-  auth_token: string,
-) {
-  return await FetchHandler(
-    new ModularRequest(`${base_url}/auth/logout`)
-      .POST
-      .ApplicationJson
-      .BearerAuth(auth_token)
-    , APIResponseEmpty
-  );
-}
+  async Register(register_request: $RegisterRequest) {
+    return await FetchHandler<$APIResponse<$RegisterResponse>>(
+      new ModularRequest(
+        `${this.base_url}/auth/register`,
+      ).POST.ApplicationJson.Body(register_request),
+      APIResponseToJSON,
+    );
+  }
 
-export async function ChangePassword(
-  base_url: string,
-  change_password_request: $ChangePasswordRequest,
-  auth_token: string,
-) {
-  return await FetchHandler(
-    new ModularRequest(`${base_url}/auth/password/change`)
-      .POST
-      .ApplicationJson
-      .BearerAuth(auth_token)
-      .Body(change_password_request)
-    , APIResponseEmpty
-  );
-}
+  async Login(login_request: $LoginRequest) {
+    let response = await FetchHandler<$APIResponse<$LoginResponse>>(
+      new ModularRequest(
+        `${this.base_url}/auth/login`,
+      ).POST.ApplicationJson.Body(login_request),
+      APIResponseToJSON,
+    );
+    if (response?.success && response.result.status === 'Success') this.auth_token = response.result.auth_token;
+    return response;
+  }
 
-export async function EnableTotp(
-  base_url: string,
-  auth_token: string
-) {
-  return await FetchHandler<$APIResponse<$EnableTotpResponse>>(
-    new ModularRequest(`${base_url}/auth/mfa/totp`)
-      .POST
-      .ApplicationJson
-      .BearerAuth(auth_token)
-    , APIResponseToJSON
-  );
-}
+  async LoginMfaStep(login_mfa_step_request: $LoginMfaStepRequest) {
+    let response = await FetchHandler<$APIResponse<$LoginResponseSuccess>>(
+      new ModularRequest(
+        `${this.base_url}/auth/login/mfa`,
+      ).POST.ApplicationJson.Body(login_mfa_step_request),
+      APIResponseToJSON,
+    );
+    if (response?.success && response.result.status === 'Success') this.auth_token = response.result.auth_token;
+    return response;
+  }
 
-export async function ConfirmEnableTotp(
-  base_url: string,
-  confirm_enable_totp_request: $ConfirmEnableTotpRequest,
-  auth_token: string
-) {
-  return await FetchHandler(
-    new ModularRequest(`${base_url}/auth/mfa/totp/confirm`)
-      .POST
-      .ApplicationJson
-      .BearerAuth(auth_token)
-      .Body(confirm_enable_totp_request)
-    , APIResponseEmpty
-  )
-}
+  async Logout() { // Logs out current session
+    if (!this.auth_token) await this.PromptLogin();
+    return await FetchHandler(
+      new ModularRequest(
+        `${this.base_url}/auth/logout`,
+      ).POST.ApplicationJson.BearerAuth(this.auth_token),
+      APIResponseEmpty,
+    );
+  }
 
-export async function DisableTotp(
-  base_url: string,
-  disable_totp_request: $DisableTotpRequest,
-  auth_token: string
-) {
-  return await FetchHandler(
-    new ModularRequest(`${base_url}/auth/mfa/totp`)
-      .DELETE
-      .ApplicationJson
-      .BearerAuth(auth_token)
-      .Body(disable_totp_request)
-    , APIResponseEmpty
-  )
-}
+  async ChangePassword(
+    change_password_request: $ChangePasswordRequest,
+  ) {
+    if (!this.auth_token) await this.PromptLogin();
+    return await FetchHandler(
+      new ModularRequest(
+        `${this.base_url}/auth/password/change`,
+      ).POST.ApplicationJson.BearerAuth(this.auth_token).Body(
+        change_password_request,
+      ),
+      APIResponseEmpty,
+    );
+  }
 
-export async function GetSessions(
-  base_url: string,
-  auth_token: string,
-) {
-  return await FetchHandler<$APIResponse<$SessionResponse>>(
-    new ModularRequest(`${base_url}/auth/sessions`)
-      .GET
-      .ApplicationJson
-      .BearerAuth(auth_token)
-    , APIResponseToJSON
-  )
-}
+  async EnableTotp() {
+    if (!this.auth_token) await this.PromptLogin();
+    return await FetchHandler<$APIResponse<$EnableTotpResponse>>(
+      new ModularRequest(
+        `${this.base_url}/auth/mfa/totp`,
+      ).POST.ApplicationJson.BearerAuth(this.auth_token),
+      APIResponseToJSON,
+    );
+  }
 
-export async function DisableSession(
-  base_url: string,
-  session: string,
-  auth_token: string,
-) {
-  return await FetchHandler(
-    new ModularRequest(`${base_url}/auth/sessions/${session}`)
-      .DELETE
-      .ApplicationJson
-      .BearerAuth(auth_token)
-    , APIResponseEmpty
-  )
-}
+  async ConfirmEnableTotp(
+    confirm_enable_totp_request: $ConfirmEnableTotpRequest,
+  ) {
+    if (!this.auth_token) await this.PromptLogin();
+    return await FetchHandler(
+      new ModularRequest(
+        `${this.base_url}/auth/mfa/totp/confirm`,
+      ).POST.ApplicationJson.BearerAuth(this.auth_token).Body(
+        confirm_enable_totp_request,
+      ),
+      APIResponseEmpty,
+    );
+  }
 
-export async function DisableAllSessions(
-  base_url: string,
-  auth_token: string,
-) {
-  return await FetchHandler(
-    new ModularRequest(`${base_url}/auth/sessions`)
-      .DELETE
-      .ApplicationJson
-      .BearerAuth(auth_token)
-    , APIResponseEmpty
-  )
+  async DisableTotp(
+    disable_totp_request: $DisableTotpRequest,
+  ) {
+    if (!this.auth_token) await this.PromptLogin();
+    return await FetchHandler(
+      new ModularRequest(
+        `${this.base_url}/auth/mfa/totp`,
+      ).DELETE.ApplicationJson.BearerAuth(this.auth_token).Body(
+        disable_totp_request,
+      ),
+      APIResponseEmpty,
+    );
+  }
+
+  async GetSessions() {
+    if (!this.auth_token) await this.PromptLogin();
+    return await FetchHandler<$APIResponse<$SessionResponse>>(
+      new ModularRequest(
+        `${this.base_url}/auth/sessions`,
+      ).GET.ApplicationJson.BearerAuth(this.auth_token),
+      APIResponseToJSON,
+    );
+  }
+
+  async DisableSession(session: string) { // Log out specific session
+    if (!this.auth_token) await this.PromptLogin();
+    return await FetchHandler(
+      new ModularRequest(
+        `${this.base_url}/auth/sessions/${session}`,
+      ).DELETE.ApplicationJson.BearerAuth(this.auth_token),
+      APIResponseEmpty,
+    );
+  }
+
+  async DisableOtherSessions() {
+    if (!this.auth_token) await this.PromptLogin();
+    return await FetchHandler(
+      new ModularRequest(
+        `${this.base_url}/auth/sessions`,
+      ).DELETE.ApplicationJson.BearerAuth(this.auth_token),
+      APIResponseEmpty,
+    );
+  }
+
+  async DisableAllSessions() {
+    if (!this.auth_token) await this.PromptLogin();
+    return await this.DisableOtherSessions().then(this.Logout);
+  }
 }
