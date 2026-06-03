ad3e92a39e0533be819fff0cb7c7136fa7eeb699
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2026-06-02 23:04:17 -0500

    feat: implement API in client class
    Signed-off-by: Neo <77405346+networkpenetrationtester@users.noreply.github.com>

--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,23 @@
+import { API } from "./api_wrapper.ts";
+
 export interface KestrelClientOptions {
-  base_url?: string;
+  base_url: string;
 }
 
 export class KestrelClient {
   private base_url: string;
+  private api: API;
 
-  constructor(options: KestrelClientOptions = {}) {
+  constructor(options: KestrelClientOptions) {
     this.base_url = options.base_url ?? 'https://kestrel.chat/api';
+    this.api = new API(options.base_url);
   }
 
-  public async ping(): Promise<string> {
+  public async Ping(): Promise<string> {
     return 'pong';
   }
 
-  public getApiUrl(): string {
+  public GetBaseURL(): string {
     return this.base_url;
   }
 }
